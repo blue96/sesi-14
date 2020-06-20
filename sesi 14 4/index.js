d14d94afa6b54a6df0e9514cac021a9aff65e6f0
blue96 <pascalfelim@yahoo.com>
2020-06-20 13:57:42 +0700

    latihan 4

---
+++ sesi 14 4/index.js
@@ -0,0 +1,11 @@
+import {isAritmathicProgrression} from './isAritmathicProgression.js'
+
+let hasil = document.getElementById('hasil')
+let txt = ''
+txt += `isAritmathicProgrression([1,2,3,4,5]) = ${isAritmathicProgrression([1,2,3,4,5])}<br>`
+txt += `isAritmathicProgrression([2,4,6,12,24]) = ${isAritmathicProgrression([2,4,6,12,24])}<br>`
+txt += `isAritmathicProgrression([2,4,6,8]) = ${isAritmathicProgrression([2,4,6,8])}<br>`
+txt += `isAritmathicProgrression([2,6,18,54]) = ${isAritmathicProgrression([2,6,18,54])}<br>`
+txt += `isAritmathicProgrression([1,2,3,4]) = ${isAritmathicProgrression([1,2,3,4])}<br>`
+hasil.innerHTML = txt
+
---
+++ sesi 14 4/isAritmathicProgression.js
@@ -0,0 +1,15 @@
+function isAritmathicProgrression(arrNumb){
+    let val = true
+    let bBase = arrNumb[1]-arrNumb[0]
+
+    for (let i = 1; i < arrNumb.length; i++){
+        if (arrNumb[i]-arrNumb[i-1] != bBase){
+            val = false
+        }
+    }
+    return val
+}
+
+export {
+    isAritmathicProgrression,
+}
